12cc88bc88f4348ad2dc4bc5b2c6635d3b8259b7
Mayank <vijaymaynk2003@gmail.com>
2026-06-26 22:06:45 +0530

    rename the folder

--- introduction-to-rag/src/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from src.models.users import User
-from src.models.documents import Document, ProcessingStatus
-from src.models.chats import ChatSession
-from src.models.messages import Message, MessageRole
-
